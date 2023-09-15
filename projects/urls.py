811afbc4981c07404311fac05afac3588e1e1bc0
Abinash Kalita <kalitaabi123@gmail.com>
2023-09-15 14:07:14 +0530

    Second Commmit

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.projects, name="projects"),
+    path('project/<str:pk>/',views.project, name="project"),
+] 
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+projectList = [
+    {
+        'id':'1',
+        'title': "ecommerce website",
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id':'2',
+        'title': "Portfolio website",
+        'description': 'Fully functional portfolio website'
+    },
+    {
+        'id':'3',
+        'title': "social network",
+        'description': 'Fully functional social network website'
+    },
+]
+
+def projects(request):
+    page = 'projects'
+    number = 10
+    context = {'page':page,'number':number, 'projects':projectList}
+    return render(request,'projects/projects.html', context )
+
+def project(request, pk):
+    projectObj = None
+    for i in projectList:
+        if i['id'] == pk:
+            projectObj =i
+    return render(request,'projects/single-project.html', {'project':projectObj})
