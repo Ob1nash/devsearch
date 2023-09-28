fe510d13803e322c3f3bff86c49bf988ca902945
Abinash Kalita <kalitaabi123@gmail.com>
2023-09-28 13:11:26 +0530

    Review and Tag added

--- projects/models.py
+++ projects/models.py
@@ -1,30 +1,53 @@
 from django.db import models
 import uuid
+
+from django.db.models.deletion import CASCADE
+# from users.models import Profile
 # Create your models here.
 
+
 class Project(models.Model):
-    title= models.CharField(max_length=200)
-    description = models.TextField(null=True, blank= True)
-    demo_link= models.CharField(max_length=2000,null=True, blank= True)
-    source_link = models.CharField(max_length=2000,null=True, blank=True)
+    # owner = models.ForeignKey(
+    #     Profile, null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id= models.UUIDField(default=uuid.uuid4,unique=True, primary_key=True,editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
-    
-    class Review(models.Model):
-        VOTE_TYPE = (
-            ('up', 'Up Vote'),
-        )
-        #owner=
-        project= models.ForeignKey(Project, on_delete= models.CASCADE)
-        body = models.TextField(null=True, blank=True)
-        value = models.CharField(max_length = 200, choices=VOTE_TYPE)
-        created = models.DateTimeField(auto_now_add=True)
-        id= models.UUIDField(default=uuid.uuid4,unique=True, primary_key=True,editable=False)
 
-        
 
-        def __str__(self):
-             return self.value
+class Review(models.Model):
+
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'down Vote'),
+    )
+    # owner =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+
+def __str__(self):
+    return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+# from .forms import ProjectForm, ReviewForm
 from django.http import HttpResponse
+from .models import Project, Tag
 
 projectList = [
     {
