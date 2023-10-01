9b79026343d465c915a4798bcb572a303c68e3ed
Abinash Kalita <kalitaabi123@gmail.com>
2023-10-02 00:45:43 +0530

    Created Modelform

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('',views.projects, name="projects"),
     path('project/<str:pk>/',views.project, name="project"),
+
+    path('create-project/',views.createProject,name="create-project"),
+
+
 ] 
--- projects/views.py
+++ projects/views.py
@@ -2,24 +2,25 @@ from django.shortcuts import render
 # from .forms import ProjectForm, ReviewForm
 from django.http import HttpResponse
 from .models import Project, Tag
+from .forms import ProjectForm 
 
-projectList = [
-    {
-        'id':'1',
-        'title': "ecommerce website",
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id':'2',
-        'title': "Portfolio website",
-        'description': 'Fully functional portfolio website'
-    },
-    {
-        'id':'3',
-        'title': "social network",
-        'description': 'Fully functional social network website'
-    },
-]
+# projectList = [
+#     {
+#         'id':'1',
+#         'title': "ecommerce website",
+#         'description': 'Fully functional ecommerce website'
+#     },
+#     {
+#         'id':'2',
+#         'title': "Portfolio website",
+#         'description': 'Fully functional portfolio website'
+#     },
+#     {
+#         'id':'3',
+#         'title': "social network",
+#         'description': 'Fully functional social network website'
+#     },
+# ]
 
 def projects(request):
     projects = Project.objects.all()
@@ -30,3 +31,8 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
 
     return render(request,'projects/single-project.html', {'project':projectObj })
+
+def createProject(request):
+    form = ProjectForm()
+    context ={'form': form}
+    return render(request, "projects/project_form.html",context)
