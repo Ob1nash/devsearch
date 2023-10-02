c375c75434a10e099b61602b7868a781d8336f00
Abinash Kalita <kalitaabi123@gmail.com>
2023-10-02 15:24:33 +0530

    create operation done

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # from .forms import ProjectForm, ReviewForm
 from django.http import HttpResponse
 from .models import Project, Tag
@@ -34,5 +34,12 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+
+    if request.method == 'POST':
+        # print(request.POST)
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
     context ={'form': form}
     return render(request, "projects/project_form.html",context)
