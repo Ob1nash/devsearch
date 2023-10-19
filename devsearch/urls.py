98716d425595bc420129b1ac76fa673aa41f0075
Abinash Kalita <kalitaabi123@gmail.com>
2023-10-20 02:59:54 +0530

    user profile page started

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -22,7 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+
+    path('', include('users.urls'))
 
 ]
 
