120bee36f8d4d86b3f085f278a1704c528498a02
Abinash Kalita <kalitaabi123@gmail.com>
2023-10-24 01:20:45 +0530

    Signals created, user and profile linked update nd delete

--- users/models.py
+++ users/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid 
+
 from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 # Create your models here.
 
 
@@ -39,14 +42,24 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
-    
-def profileUpdated(sender, instance, created, **kwargs):
-    print('Profile Saved!')
-    print('Instance:', instance)
-    print('CREATED:',created)
+
+#@receiver (post_save, sender= Profile)  
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile =  Profile.objects.create(
+            user = user,
+            username = user.username,
+            email= user.email,
+            name= user.first_name,
+        )
 
 def deleteUser(sender, instance, **kwargs):
-    print('Deleting user...')
+    user = instance.user
+    user.delete() 
 
-post_save.connect(profileUpdated, sender=Profile)
+post_save.connect(createProfile, sender=User)
+
+
+
 post_delete.connect(deleteUser, sender=Profile)
